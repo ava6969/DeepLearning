4ce7153b8a4f05cc61f55b246528f9e2a032aab8
dewe <ava6969@rit.edu>
2022-04-16 12:40:29 -0400

    current bug

--- wrappers/adapters.h
+++ wrappers/adapters.h
@@ -12,7 +12,6 @@ namespace sam_dn{
     class Conv2DPositionEncodeImpl: public ModuleWithSizeInfoImpl{
 
     public:
-        Conv2DPositionEncodeImpl()=default;
 
         explicit Conv2DPositionEncodeImpl(BaseModuleOption opt): ModuleWithSizeInfoImpl(opt){
             auto in = opt.dict_opt[opt.input];
